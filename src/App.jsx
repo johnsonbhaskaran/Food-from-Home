968303d9f0ef1bf23562722266d85865e23b92bf
Johnson Bhaskaran <johnsonb@live.in>
2025-06-04 00:12:31 +0530

    Initial commit with basic boilerplate.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className='p-5 bg-fuchsia-700 text-white text-5xl text-center'>FOOD from HOME</div>
+    </>
+  );
+}
+
+export default App;
