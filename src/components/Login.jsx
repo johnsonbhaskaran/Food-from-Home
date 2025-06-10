f36c485b29002f00b42c9cea16f274bd2684eb7a
Johnson Bhaskaran <johnsonb@live.in>
2025-06-10 23:59:58 +0530

    Form input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,24 @@ import React from "react";
 const Login = () => {
   return (
     <div className='flex w-full h-screen bg-slate-300'>
-      <div className='w-full md:w-1/2 flex items-center justify-center'>Login</div>
-      <div className='hidden md:w-1/2 bg-amber-300 md:flex items-center justify-center'>Login</div>
+      <div className='w-full md:w-1/2 flex items-center justify-center'>
+        <div>
+          <h1>Welcome Back</h1>
+          <p>Please enter your details</p>
+          <div>
+            <div>
+              <label htmlFor=''>Email</label>
+              <input
+                className=''
+                placeholder='Enter your email'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='hidden md:w-1/2 bg-amber-300 md:flex items-center justify-center'>
+        Graphic image goes here
+      </div>
     </div>
   );
 };
