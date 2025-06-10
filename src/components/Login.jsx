0a2886d4e5a6b16cab035eb502ed5f128f98599c
Johnson Bhaskaran <johnsonb@live.in>
2025-06-10 23:53:22 +0530

    login screen base code for mobile and tablet

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Login = () => {
   return (
     <div className='flex w-full h-screen bg-slate-300'>
-      <div>Login</div>
+      <div className='w-full md:w-1/2 flex items-center justify-center'>Login</div>
+      <div className='hidden md:w-1/2 bg-amber-300 md:flex items-center justify-center'>Login</div>
     </div>
   );
 };
