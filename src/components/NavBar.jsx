5737bd98676e23129dc3576e88daf227f1dc8f2f
Johnson Bhaskaran <johnsonb@live.in>
2025-06-12 23:49:18 +0530

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const NavBar = () => {
   const Menus = [
     { name: "Home", icon: "home-outline", dis: "translate-x-0" },
-    { name: "Profile", icon: "person-outline", dis: "translate-x-16" },
     { name: "Message", icon: "chatbubble-outline", dis: "translate-x-32" },
-    { name: "Photos", icon: "camera-outline", dis: "translate-x-48" },
+    { name: "Favorites", icon: "person-outline", dis: "translate-x-16" },
+    { name: "Cart", icon: "camera-outline", dis: "translate-x-48" },
     { name: "Settings", icon: "settings-outline", dis: "translate-x-64" },
   ];
 
@@ -14,9 +14,12 @@ const NavBar = () => {
       <div className='bg-white max-h-[4.4rem] px-6 rounded-t-xl'>
         <ul className='flex relative'>
           {Menus.map((menu, i) => (
-            <li key={i}>
-              <a href='#'>
-                <span>
+            <li
+              key={i}
+              className='w-16'
+            >
+              <a className='flex flex-col text-center'>
+                <span className='text-xl cursor-pointer'>
                   <ion-icon name={menu.icon}></ion-icon>
                 </span>
                 <span>{menu.name}</span>
