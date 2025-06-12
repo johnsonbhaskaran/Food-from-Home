6d555c890d4f3ac8af58a0f6b9154e7d5b921430
Johnson Bhaskaran <johnsonb@live.in>
2025-06-12 23:44:10 +0530

    Navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const Menus = [
+    { name: "Home", icon: "home-outline", dis: "translate-x-0" },
+    { name: "Profile", icon: "person-outline", dis: "translate-x-16" },
+    { name: "Message", icon: "chatbubble-outline", dis: "translate-x-32" },
+    { name: "Photos", icon: "camera-outline", dis: "translate-x-48" },
+    { name: "Settings", icon: "settings-outline", dis: "translate-x-64" },
+  ];
+
+  return (
+    <div className='w-full h-screen bg-gray-950 flex justify-center items-center'>
+      <div className='bg-white max-h-[4.4rem] px-6 rounded-t-xl'>
+        <ul>
+          {Menus.map((menu, i) => (
+            <li key={i}>
+              <a href='#'>
+                <span>
+                  <ion-icon name={menu.icon}></ion-icon>
+                </span>
+                <span>{menu.name}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
