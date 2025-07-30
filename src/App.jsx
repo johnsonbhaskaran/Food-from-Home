4a380ec9c14862e959603d1038687c0e16a02138
Johnson Bhaskaran <johnsonb@live.in>
2025-07-30 22:09:44 +0530

    Chef Pages folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import FeatherIcon from "feather-icons-react";
+import ToggleIconContainer from "./ToggleIconContainer";
 
 function App() {
   return (
     <>
+      <h1 className='text-6xl'>Tittle</h1>
       <FeatherIcon icon='copy' size='240' />
+      <FeatherIcon icon='heart' fill='red' size='240' />
+      <ToggleIconContainer />
     </>
   );
 }
---
+++ src/ToggleIconContainer.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import FeatherIcon from "feather-icons-react";
+
+const ToggleIconContainer = () => {
+  const [icon, setIcon] = useState("x");
+
+  return (
+    <div>
+      <FeatherIcon icon={icon} />
+      <ul>
+        <li>
+          <button onClick={() => setIcon("x")}>Make the Icon an X</button>
+        </li>
+        <li>
+          <button onClick={() => setIcon("anchor")}>Make the Icon an Anchor</button>
+        </li>
+        <li>
+          <button onClick={() => setIcon("box")}>Make the Icon a box</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default ToggleIconContainer;
