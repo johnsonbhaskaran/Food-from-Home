84cabbf684d0b81e2e2ee935fd59f90791b93f18
Johnson Bhaskaran <johnsonb@live.in>
2025-07-31 22:12:33 +0530

    Feather Icon setup and testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='bg-[#0E0A0F]'>
       <h1 className='text-6xl'>Tittle</h1>
-      <FeatherIcon icon='copy' size='240' />
+      {/* <FeatherIcon icon='copy' size='240' /> */}
       <FeatherIcon icon='heart' fill='red' size='240' />
       <ToggleIconContainer />
     </div>
