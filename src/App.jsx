b9604a6773db834a3eb3ac07f7785776efc1b804
Johnson Bhaskaran <johnsonb@live.in>
2025-07-31 22:11:33 +0530

    Brand colors - Background color for dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import ToggleIconContainer from "./ToggleIconContainer";
 
 function App() {
   return (
-    <>
+    <div className='bg-[#0E0A0F]'>
       <h1 className='text-6xl'>Tittle</h1>
       <FeatherIcon icon='copy' size='240' />
       <FeatherIcon icon='heart' fill='red' size='240' />
       <ToggleIconContainer />
-    </>
+    </div>
   );
 }
 
