9fbc9b0571affef88f71d9c8b0a0b50449d98268
Johnson Bhaskaran <johnsonb@live.in>
2025-08-02 21:36:43 +0530

    Location Home screen. Search field component need to be fixed.

--- src/components/commonComponents/SearchDropdown.jsx
+++ src/components/commonComponents/SearchDropdown.jsx
@@ -1,4 +1,12 @@
 const SearchDropdown = () => {
-  return <div>SearchDropdown</div>;
+  return (
+    <div>
+      <div
+        className='relative bg-brand-fore-dark px-6 py-6 rounded-full outline-0 w-full text-brand-text-light'
+        type='text'
+        placeholder='{value}'
+      />
+    </div>
+  );
 };
 export default SearchDropdown;
