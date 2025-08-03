a2f18642adc5197d9b26d6ad4953e1f3bcffffb1
Johnson Bhaskaran <johnsonb@live.in>
2025-08-03 16:58:09 +0530

    Account Settings screen completed

--- src/components/commonComponents/TextareaField.jsx
+++ src/components/commonComponents/TextareaField.jsx
@@ -1,9 +1,9 @@
-const TextareaField = () => {
+const TextareaField = ({ value }) => {
   return (
     <div>
       <textarea
-        className='bg-brand-fore-dark px-6 py-4 rounded-xl outline-0 w-full text-brand-text-light resize-none'
-        placeholder='hoi'
+        className='bg-brand-fore-dark px-6 py-4 rounded-xl outline-0 w-full h-32 text-brand-text-light leading-6 resize-none'
+        placeholder={value}
       />
     </div>
   );
--- src/pages/customerPages/CustomerAccountSettings.jsx
+++ src/pages/customerPages/CustomerAccountSettings.jsx
@@ -1,23 +1,29 @@
+import BtnGradientBorder from "../../components/commonComponents/BtnGradientBorder";
 import InputField from "../../components/commonComponents/InputField";
 import TextareaField from "../../components/commonComponents/TextareaField";
 
 const CustomerAccountSettings = () => {
   const customer = {
-    name: "Johnson Bhaskaran",
+    name: "Johnson B",
   };
 
   return (
-    <div className='flex flex-col items-center mx-6'>
+    <div className='mx-auto md:max-w-lg container'>
       <div className='mt-14'>
-        <h1 className='font-bold text-3xl'>Account Settings</h1>
+        <h1 className='font-bold text-3xl text-center'>Account Settings</h1>
       </div>
-      <div className='mt-12 mr-auto'>
+      <div className='mx-6 mt-12'>
         <p className='text-xl'>Welcome, {customer.name}</p>
       </div>
-      <div className='space-y-1 mt-8 min-w-md'>
-        <InputField value={"Full Name"} type={"text"} />
-        <InputField value={"Email"} type={"text"} />
-        <TextareaField value={"Email"} type={"text"} />
+      <div className='space-y-1 mx-6 mt-8'>
+        <InputField value={customer.name} type={"text"} />
+        <InputField value={"Password"} type={"password"} />
+        <InputField value={"Contact number"} type={"tel"} />
+        <InputField value={"Profile picture"} type={"text"} />
+        <TextareaField value={"Delivery address"} type={"text"} />
+      </div>
+      <div className='block mx-6 my-4 text-right'>
+        <BtnGradientBorder value={"Save"} />
       </div>
     </div>
   );
