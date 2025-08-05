152455df7ff53aa0223af4f45958e17af70c170a
Johnson Bhaskaran <johnsonb@live.in>
2025-08-05 17:09:08 +0530

    Toggling functionality completed

--- src/components/commonComponents/ToggleSwitch.jsx
+++ src/components/commonComponents/ToggleSwitch.jsx
@@ -1,7 +1,18 @@
-const ToggleSwitch = ({ isDark = false }) => {
+import { useState } from "react";
+
+const ToggleSwitch = ({ isDark }) => {
+  const [enabled, setEnabled] = useState(isDark);
+
+  const handleToggle = () => {
+    setEnabled((prev) => !prev);
+  };
+
   return (
-    <div className='flex items-center gap-x-1 p-0.5 rounded-full w-12 h-6 bg-brand-text-light cursor-pointer'>
-      <div className={`rounded-full size-5 bg-brand-text-dark ${isDark && "ms-6"}`} />
+    <div
+      onClick={() => handleToggle()}
+      className='flex items-center gap-x-1 p-0.5 rounded-full w-12 h-6 bg-brand-text-light cursor-pointer'
+    >
+      <div className={`rounded-full size-5 bg-brand-text-dark ${enabled && "ms-6"}`} />
     </div>
   );
 };
