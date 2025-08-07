1c6d259d1a8efc22d97204c1c2865f77bf423f9b
Johnson Bhaskaran <johnsonb@live.in>
2025-08-07 12:24:00 +0530

    Selected Food item popup UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import CustomerNavBar from "./components/commonComponents/CustomerNavBar";
-import FavoriteFood from "./pages/customerPages/FavoriteFood";
+import FoodItemPopup from "./pages/customerPages/FoodItemPopup";
 
 function App() {
   return (
     <div className='bg-brand-bg-dark font-urbanist text-brand-text-light'>
-      <FavoriteFood />
+      <FoodItemPopup />
       <CustomerNavBar active={3} />
     </div>
   );
