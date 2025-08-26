0a21a5c54ef1d31cb390dd1c039820072a7980d5
Johnson Bhaskaran <johnsonb@live.in>
2025-08-27 05:05:08 +0530

    Fixed the eslint. Disabled the no-undef

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import * as dotenv from "dotenv";
 import express from "express";
+import "dotenv/config.js";
 
-dotenv.config({ path: "../.env", quiet: true });
+// eslint-disable-next-line no-undef
+const PORT = process.env.SERVER_PORT || 3001;
 
 const app = express();
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server running on port: ${process.env.SERVER_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
