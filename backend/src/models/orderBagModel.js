ebfd54997df7749432475872736c508b566a0272
Johnson Bhaskaran <johnsonb@live.in>
2025-08-27 21:07:16 +0530

    Drafted mongo DB schema for Order Bag

---
+++ backend/src/models/orderBagModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const orderBagSchema = new mongoose.Schema({});
+
+export const OrderBag = mongoose.model("OrderBag", orderBagSchema);
+
+/* -----------------------------------------------------------------/
+                    ** OrderBag
+                   itemName, itemQuantity, itemSize, itemPrice, itemInstruction, itemOrderID, itemOrderTime, totalPrice
+/------------------------------------------------------------------*/
