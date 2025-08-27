dfad695e533334c0eb75ed99cacf3182d1241e3c
Johnson Bhaskaran <johnsonb@live.in>
2025-08-28 01:29:40 +0530

    Chat Schema design drafted

---
+++ backend/src/models/chatModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({});
+
+export const Chat = mongoose.model("Chat", chatSchema);
+
+/* -----------------------------------------------------------------/
+                    ** Chat
+                   customer [messageSent, dateTime, readStatus, storeID, orderID]
+                   chef [messageSent, dateTime, readStatus, storeID, orderID]
+/------------------------------------------------------------------*/
