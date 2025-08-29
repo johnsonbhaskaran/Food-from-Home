49d52ddd3a144fe892b451143918309c0a8a3537
Johnson Bhaskaran <johnsonb@live.in>
2025-08-29 16:22:06 +0530

    chefModel and chatModel updated

--- backend/src/models/chatModel.js
+++ backend/src/models/chatModel.js
@@ -1,11 +1,30 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({});
+const chatSchema = new mongoose.Schema({
+  customer: [
+    {
+      messageContent: { type: String, default: "" },
+      dateTime: { type: Date, required: true, default: Date.now },
+      readReciept: { type: Boolean },
+      storeID: { type: mongoose.Schema.Types.ObjectId, ref: "Store" },
+      orderID: { type: mongoose.Schema.Types.ObjectId, ref: "orderBagID" },
+    },
+  ],
+  chef: [
+    {
+      messageContent: { type: String, default: "" },
+      dateTime: { type: Date, required: true, default: Date.now },
+      readReciept: Boolean,
+      customerID: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
+      orderID: { type: mongoose.Schema.Types.ObjectId, ref: "orderBagID" },
+    },
+  ],
+});
 
 export const Chat = mongoose.model("Chat", chatSchema);
 
 /* -----------------------------------------------------------------/
                     ** Chat
-                   customerChat [messageSent, dateTime, readStatus, storeID, orderID]
-                   chefChat [messageSent, dateTime, readStatus, customerID, orderID]
+                   customerChat [messageContent, dateTime, readReciept, storeID, orderID]
+                   chefChat [messageContent, dateTime, readStatus, customerID, orderID]
 /------------------------------------------------------------------*/
--- backend/src/models/chefModel.js
+++ backend/src/models/chefModel.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
+import { ChefStore } from "./chefStoreModel";
 
-const chefSchema = new mongoose.Schema({});
+const chefSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: { type: Number, required: true },
+  profileImage: String,
+  storeID: { type: mongoose.Schema.Types.ObjectId, ref: "ChefStore" },
+  specialty: String,
+});
 
 export const Chef = mongoose.model("Chef", chefSchema);
 
 /* -----------------------------------------------------------------/
                     ** CHEF
-                   fullname, password, email, phone, profileImage, store, speciality
+                   firstname, lastname, password, email, phone, profileImage, store, speciality
 /------------------------------------------------------------------*/
