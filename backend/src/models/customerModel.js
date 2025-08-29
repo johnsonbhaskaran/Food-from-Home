aa2f686bce49d5241580383cf2bc37fdb27f95f8
Johnson Bhaskaran <johnsonb@live.in>
2025-08-29 18:28:41 +0530

    customer model schema completed

--- backend/src/models/customerModel.js
+++ backend/src/models/customerModel.js
@@ -1,10 +1,61 @@
 import mongoose from "mongoose";
 
-const customerSchema = new mongoose.Schema({});
+const customerSchema = new mongoose.Schema(
+  {
+    customer: {
+      firstname: { type: String, required: true },
+      lastname: { type: String, required: true },
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+      phone: { type: Number, required: true },
+      profileImage: String,
+      address: {
+        shipping: {
+          line1: {
+            type: String,
+            required: true,
+          },
+          line2: {
+            type: String,
+          },
+          city: String,
+          state: String,
+          pincode: Number,
+          required: true,
+        },
+        billing: {
+          line1: {
+            type: String,
+            required: true,
+          },
+          line2: {
+            type: String,
+          },
+          city: String,
+          state: String,
+          pincode: Number,
+        },
+      },
+      location: {
+        zone: { type: Number, required: true },
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true },
+      },
+      specialty: String,
+    },
+    paymentMode: { type: String, enum: ["upi", "credit", "cod"], required: true },
+    orderHistory: [{ item: { type: mongoose.Schema.Types.ObjectId, ref: "OrderBag" } }],
+    paymentHistory: [{ item: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerPayment" } }],
+    chatHistory: [{ item: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" } }],
+    favouriteFoodList: [{ item: { type: mongoose.Schema.Types.ObjectId, ref: "FoodItem" } }],
+    bagList: [{ item: { type: mongoose.Schema.Types.ObjectId, ref: "FoodItem" } }],
+  },
+  { timestamps: true }
+);
 
 export const Customer = mongoose.model("Customer", customerSchema);
 
 /* -----------------------------------------------------------------/
                     ** CUSTOMER
-                   fullname, profileImage, address [shipping, billing], location, zone, phone, email, password, orderHistory, paymentHisory, chatHistory, favouriteFoodlist, bagList, paymentMode [UPI, Credit, COD]
+                   firstname, lastname, profileImage, address [shipping, billing], location, zone, phone, email, password, orderHistory, paymentHisory, chatHistory, favouriteFoodlist, bagList, paymentMode [UPI, Credit, COD]
 /------------------------------------------------------------------*/
