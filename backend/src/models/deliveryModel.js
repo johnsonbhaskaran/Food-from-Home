f8c94773dce2d003703724eba834f0b42f70599b
Johnson Bhaskaran <johnsonb@live.in>
2025-08-29 19:05:20 +0530

    delivery model schema created

--- backend/src/models/deliveryModel.js
+++ backend/src/models/deliveryModel.js
@@ -1,10 +1,28 @@
 import mongoose from "mongoose";
 
-const deliverySchema = new mongoose.Schema({});
+const deliverySchema = new mongoose.Schema(
+  {
+    delivery: {
+      date: { type: Date, required: true, default: Date.now },
+      time: { type: String, required: true }, // Extract time from full datetime.
+      status: {
+        type: String,
+        enum: ["picked", "onTheWay", "delivered", "delayed", "cancelled", "returned"],
+        required: true,
+        default: "picked",
+      },
+      orderBag: [{ item: { type: mongoose.Schema.Types.ObjectId, ref: "OrderBag" } }],
+      paymentTransactionID: [
+        { item: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerPayment" } },
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
 export const Delivery = mongoose.model("Delivery", deliverySchema);
 
 /* -----------------------------------------------------------------/
                     ** Delivery
-                   dateTime, status, itemOrderID, orderBagID, paymentTransactionID
+                   dateTime, status, itemOrderID, orderBag, paymentTransactionID
 /------------------------------------------------------------------*/
