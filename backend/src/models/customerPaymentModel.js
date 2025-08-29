ed93c760866597579565d7f3163c46bb7b1d8fbc
Johnson Bhaskaran <johnsonb@live.in>
2025-08-29 18:55:44 +0530

    customer payment schema drafted

--- backend/src/models/customerPaymentModel.js
+++ backend/src/models/customerPaymentModel.js
@@ -1,6 +1,23 @@
 import mongoose from "mongoose";
 
-const customerPaymentSchema = new mongoose.Schema({});
+const customerPaymentSchema = new mongoose.Schema(
+  {
+    payment: {
+      date: { type: Date, required: true, default: Date.now },
+      time: { type: String, required: true }, // Extract time from full datetime.
+      status: {
+        type: String,
+        enum: ["processing", "pending", "cancelled", "completed", "scheduled"],
+        required: true,
+        default: "processing",
+      },
+      amount: { type: Number, required: true, default: 0 },
+      transactionID: { type: String, required: true, default: "" },
+      orderID: { type: mongoose.Schema.Types.ObjectId, ref: "OrderBag" },
+    },
+  },
+  { timestamps: true }
+);
 
 export const CustomerPayment = mongoose.model("CustomerPayment", customerPaymentSchema);
 
