7c3da5b4abef5a08fb52dd25c091604658d779e1
Johnson Bhaskaran <johnsonb@live.in>
2025-08-30 23:46:59 +0530

    Food Item model schema to add food item to the store.

--- backend/src/models/foodItemModel.js
+++ backend/src/models/foodItemModel.js
@@ -1,10 +1,39 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const foodItemSchema = new mongoose.Schema({});
+const foodItemSchema = new mongoose.Schema(
+  {
+    foodItem: {
+      name: { type: String, required: true },
+      store: { type: Schema.Types.ObjectId, ref: "ChefStore", required: true },
+      ingredients: { type: [String], required: true },
+      category: { type: [String], required: true },
+      tags: { type: [String], required: true },
+      isVeg: { type: Boolean, required: true },
+      discount: Number,
+      size: { type: String, enum: ["small", "medium", "large", "x-large"] },
+      weight: { type: Number, min: 10, max: 2000 },
+      volume: { type: Number, min: 100, max: 1000 },
+      unit: {
+        type: String,
+        enum: ["g", "ml"],
+      },
+      images: { type: [String], required: true },
+      description: { type: String, maxlength: 200 },
+      additionalInfo: { type: String, maxlength: 200 },
+      preparationTime: { type: Number, required: true },
+      isTodaysSpecial: Boolean,
+      pricePerUnit: { type: Number, required: true },
+      starRatings: { type: Number, min: 0, max: 5 },
+      popularityRank: { type: Number, default: 0 },
+      trendingScore: { type: Number, default: 0 },
+    },
+  },
+  { timestamps: true }
+);
 
 export const FoodItem = mongoose.model("FoodItem", foodItemSchema);
 
 /* -----------------------------------------------------------------/
                     **FOOD
-                   name, store, ingredients, category, isVeg, discount, size, weight, volume, unit, images, description, additionalInfo, prepartionTime, saleCount, popularityRank, trendingScore, isTodaySpecial, tags, pricePerUnit, starRatings
+                   name, store, ingredients, category, tags, isVeg, discount, size, weight, volume, unit, images, description, additionalInfo, prepartionTime, isTodaySpecial, pricePerUnit, starRatings, popularityRank, trendingScore,
 /------------------------------------------------------------------*/
