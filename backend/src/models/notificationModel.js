835b21a36b0b260f9af8e71a8f146be4f89e9dba
Johnson Bhaskaran <johnsonb@live.in>
2025-08-31 00:18:00 +0530

    Notification model schema created. It is responsible to store all things related to customer and chef notifiactons

--- backend/src/models/notificationModel.js
+++ backend/src/models/notificationModel.js
@@ -1,11 +1,33 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const notificationSchema = new mongoose.Schema({});
+const notificationSchema = new mongoose.Schema(
+  {
+    notify: {
+      customer: {
+        name: { type: Schema.Types.ObjectId, ref: "Customer" },
+        orderStatus: { type: Schema.Types.ObjectId, ref: "ChefStore" },
+        paymentStatus: { type: Schema.Types.ObjectId, ref: "CustomerPayment" },
+        chefChat: { type: Schema.Types.ObjectId, ref: "Chat" },
+        orderBag: { type: Schema.Types.ObjectId, ref: "OrderBag" },
+        offers: [String],
+      },
+      chef: {
+        name: { type: Schema.Types.ObjectId, ref: "Chef" },
+        store: { type: Schema.Types.ObjectId, ref: "ChefStore" },
+        deliveryStatus: { type: Schema.Types.ObjectId, ref: "Delivery" },
+        customerChat: { type: Schema.Types.ObjectId, ref: "Chat" },
+        orderBag: { type: Schema.Types.ObjectId, ref: "OrderBag" },
+        payoutInfo: { type: Schema.Types.ObjectId, ref: "ChefPayout" },
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 export const Notification = mongoose.model("Notification", notificationSchema);
 
 /* -----------------------------------------------------------------/
                     ** Notification
-                    notifyCustomer [orderStatus, chefChat, orderID, offers]
-                   notifyChef [payout, deliveryStatus, customerChat, storeID]
+                    notify-Customer [name, orderStatus, chefChat, orderID, offers]
+                   notify-Chef [name, store, payoutInfo, deliveryStatus, orderID, customerChat, storeID]
 /------------------------------------------------------------------*/
