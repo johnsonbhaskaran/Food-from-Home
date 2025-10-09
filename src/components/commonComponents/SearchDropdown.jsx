237f3aecf2cf46d7978cd4fa15da2d690926857c
Johnson Bhaskaran <johnsonb@live.in>
2025-10-09 17:16:38 +0530

    Minor: div changed to input field and now the value will be the placeholder

--- src/components/commonComponents/SearchDropdown.jsx
+++ src/components/commonComponents/SearchDropdown.jsx
@@ -1,10 +1,10 @@
-const SearchDropdown = () => {
+const SearchDropdown = ({ value }) => {
   return (
     <div>
-      <div
+      <input
         className='relative bg-brand-fore-dark px-6 py-6 rounded-full outline-0 w-full text-brand-text-light'
         type='text'
-        placeholder='{value}'
+        placeholder={value}
       />
     </div>
   );
