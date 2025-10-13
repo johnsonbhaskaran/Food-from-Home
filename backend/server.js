b612c3e7f0a80b1cd23846f5a85eb63ae2275453
Johnson Bhaskaran <johnsonb@live.in>
2025-10-13 12:41:05 +0530

    Add: user auth route created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config.js";
 import cors from "cors";
 import authRoutes from "./src/routes/authRoutes.js";
 import foodListRoutes from "./src/routes/foodListRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
 import dbConnect from "./src/config/dbConnect.js";
 
 // eslint-disable-next-line no-undef
@@ -12,12 +13,13 @@ const app = express();
 dbConnect();
 
 // Middlewares
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/foodList", foodListRoutes);
+app.use("/api/users", userRoutes);
 
 // Custom Middlewares
 
