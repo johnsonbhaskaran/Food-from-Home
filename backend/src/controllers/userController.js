fd6f2db44a2efa3cdca3deace7f96a95b6430de6
Johnson Bhaskaran <johnsonb@live.in>
2025-10-14 21:31:06 +0530

    Done: update user, delete user and add user details api endpoints completed.

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,13 +1,52 @@
 import { Customer } from "../models/customerModel.js";
+import "dotenv/config.js";
 
-const getUser = (req, res) => {
+/* -----------------------------------------------------------------/
+                    * Update User *
+/------------------------------------------------------------------*/
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { newUsername } = req.body;
+
+  console.log("PUT endpoint", id, req.user);
+
+  const user = await Customer.findByIdAndUpdate(id, { username: newUsername });
+  console.log(user);
+  if (user?.isChef) return res.status(200).json("Chef details updated");
+  res.status(200).json("User details updated");
+};
+
+/* -----------------------------------------------------------------/
+                    * Add User Details *
+/------------------------------------------------------------------*/
+
+const addUserDetails = async (req, res) => {
   const { id } = req.params;
 
-  const user = Customer.findById(id);
-
-  res.status(200).json(user);
+  try {
+    const user = await Customer.findOneAndUpdate(
+      { userAuthId: id },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    console.log(user);
+    res.status(200).json({ message: "Details added to the User document", user });
+  } catch (err) {
+    console.log(err);
+    res.status(403).json("Error adding details");
+  }
 };
-const updateUser = () => {};
-const deleteUser = () => {};
 
-export { getUser, deleteUser, updateUser };
+/* -----------------------------------------------------------------/
+                    * Delete User *
+/------------------------------------------------------------------*/
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  const user = await Customer.findByIdAndDelete(id);
+  console.log(user);
+  res.status(200).json("User deleted");
+};
+
+export { deleteUser, updateUser, addUserDetails };
